97f1c3f832052ef71dcf93d207c219f29b3e1372
arham <arham@OMEGAUI-PC>
2024-12-22 13:58:58 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NakimeWindowsService
+{
+    internal class Class1
+    {
+        class StartupEntry
+        {
+            public string startTime;
+            public string endTime;
+
+            public StartupEntry(string startTime, string endTime)
+            {
+                this.startTime = startTime;
+                this.endTime = endTime;
+            }
+        }
+
+        private static List<StartupEntry> entries = new List<StartupEntry>();
+
+        public static void Main(string[] args)
+        {
+            string todaysFileStamp = "DateToFileStamp(startTime)";
+            string path = "C:\\Users\\arham\\Downloads\\" + todaysFileStamp + ".json";
+            var stream = File.Create(path);
+            var upTime = "a";
+            var downTime = "b";
+            var startupEntry = new StartupEntry(upTime, downTime);
+            entries.Add(startupEntry);
+            JsonSerializer.Serialize(stream, entries);
+            stream.Flush();
+            stream.Close();
+        }
+    }
+}
---
+++ StartupWatcher.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NakimeWindowsService
+{
+    public partial class StartupWatcher : ServiceBase
+    {
+        class StartupEntry
+        {
+            public string startTime { get; set; }
+            public string endTime { get; set; }
+        }
+
+        private List<StartupEntry> entries = new List<StartupEntry>();
+        private FileStream stream;
+        private DateTime startTime;
+
+        public StartupWatcher()
+        {
+            InitializeComponent();
+            startTime = DateTime.Now;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string todaysFileStamp = DateToFileStamp(startTime);
+            string username = "Default";
+
+            // check if AppData\Local\Nakime directory exists
+            string parentDirPath = "C:\\Users\\" + username + "\\AppData\\Local\\Nakime";
+            if (!Directory.Exists(parentDirPath))
+            {
+                Directory.CreateDirectory(parentDirPath);
+            }
+
+            string path = parentDirPath + "\\" + todaysFileStamp + ".json";
+            if (File.Exists(path))
+            {
+                var data = File.ReadAllText(path);
+                entries = JsonSerializer.Deserialize<List<StartupEntry>>(data);
+            }
+            stream = File.Create(path);
+        }
+
+        protected override void OnStop()
+        {
+            var upTime = DateToStartUpEntry(startTime);
+            var downTime = DateToStartUpEntry(DateTime.Now);
+            entries.Add(new StartupEntry() {
+                startTime = upTime,
+                endTime = downTime,
+            });
+            JsonSerializer.Serialize(stream, entries);
+            stream.Flush();
+            stream.Close();
+        }
+
+        private string DateToFileStamp(DateTime date)
+        {
+            return date.Day + "-" + date.Month + "-" + date.Year;
+        }
+
+        private string DateToStartUpEntry(DateTime date)
+        {
+            return date.Hour + ":" + date.Minute+ ":" + date.Second;
+        }
+    }
+}
